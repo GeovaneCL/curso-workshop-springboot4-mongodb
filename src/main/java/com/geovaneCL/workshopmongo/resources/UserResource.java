64500c12c8d24b1740c10f1798bbc832f55c3041
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-25 15:58:03 -0300

    Obtendo um usuario por id

--- src/main/java/com/geovaneCL/workshopmongo/resources/UserResource.java
+++ src/main/java/com/geovaneCL/workshopmongo/resources/UserResource.java
@@ -1,4 +1,4 @@
-package com.geovaneCL.workshopmongo.resourcess; // ou o pacote correto do seu controller
+package com.geovaneCL.workshopmongo.resources; // ou o pacote correto do seu controller
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
+++ src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.geovaneCL.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.geovaneCL.workshopmongo.domain.User;
 import com.geovaneCL.workshopmongo.repository.UserRepository;
+import com.geovaneCL.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository useRepo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return useRepo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = useRepo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
