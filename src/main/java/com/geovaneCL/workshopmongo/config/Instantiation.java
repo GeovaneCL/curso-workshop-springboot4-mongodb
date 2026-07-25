3e5bdd7c6a7006a5673733ab3a54054049b00a42
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-25 18:59:42 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/geovaneCL/workshopmongo/config/Instantiation.java
+++ src/main/java/com/geovaneCL/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.geovaneCL.workshopmongo.domain.Post;
 import com.geovaneCL.workshopmongo.domain.User;
+import com.geovaneCL.workshopmongo.dto.AuthorDTO;
 import com.geovaneCL.workshopmongo.repository.PostRepository;
 import com.geovaneCL.workshopmongo.repository.UserRepository;
 
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
          User alex = new User(null, "Alex Green", "alex@gmail.com");
          User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018 19:30"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!",maria);
-        Post post2 = new Post(null, sdf.parse("21/03/2018 19:30"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!",maria);
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2018 19:30"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018 19:30"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+
+
          postRepository.saveAll(Arrays.asList(post1, post2));
 
         }
--- src/main/java/com/geovaneCL/workshopmongo/domain/Post.java
+++ src/main/java/com/geovaneCL/workshopmongo/domain/Post.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.geovaneCL.workshopmongo.dto.AuthorDTO;
 
 @JsonPropertyOrder({ "id", "date", "title", "body", "author" })
 @Document(collation = "post")
@@ -18,13 +19,13 @@ public class Post implements Serializable{
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post(){
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -66,12 +67,12 @@ public class Post implements Serializable{
     }
     
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
