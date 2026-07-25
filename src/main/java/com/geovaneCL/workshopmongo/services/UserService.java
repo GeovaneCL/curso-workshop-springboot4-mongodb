e5a2265d5271e4ebbeae94936e47acfab3e581e3
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-25 17:06:31 -0300

    Deleção de usuario com DELETE

--- src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
+++ src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
         return useRepo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id);
+        useRepo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDTO){
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
