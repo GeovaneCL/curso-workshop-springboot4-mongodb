3afcaeded3128fdabbe076bc7e01a425d44afb87
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-25 16:33:26 -0300

    Insercao de usuario com POST

--- src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
+++ src/main/java/com/geovaneCL/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.geovaneCL.workshopmongo.domain.User;
+import com.geovaneCL.workshopmongo.dto.UserDTO;
 import com.geovaneCL.workshopmongo.repository.UserRepository;
 import com.geovaneCL.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = useRepo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return useRepo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
